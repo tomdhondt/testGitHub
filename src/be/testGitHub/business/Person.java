ba04e60f52a5e0c24b626134504a6ec37f208484
tomdhondt <tom.dhondt@hotmail.com>
2014-05-05 21:11:13 +0200

    FirstCommit
    First commit for the project

---
+++ src/be/testGitHub/business/Person.java
@@ -0,0 +1,35 @@
+package be.testGitHub.business;
+
+public class Person {
+	/* instance members */
+	private String name;
+	private String lastName;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
